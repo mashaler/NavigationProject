84f39b1af484906a9fc89ff9c78d355418f5e50e
mashaler <mashalerolva@gmail.com>
2023-05-19 16:14:40 +0200

    first commit

--- NavigationProject/HomeVC.swift
+++ NavigationProject/HomeVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeVC.swift
 //  NavigationProject
 //
 //  Created by DA MAC  M1 154 on 2023/05/19.
@@ -7,13 +7,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
+    
 
-
+    @IBAction func ButtonPressed(_ sender: UIButton) {
+//        performSegue(withIdentifier: <#T##String#>, sender: <#T##Any?#>)
+    }
+    
 }
-
